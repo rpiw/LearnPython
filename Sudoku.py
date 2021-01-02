97bd6878cc4429b3fe6d49fe72eecd173f479d31
rpiw <radoslawp95@gmail.com>
2021-01-02 21:48:46 +0100

    Fix lack of update in attributes.

--- Sudoku.py
+++ Sudoku.py
@@ -9,9 +9,20 @@ class Sudoku:
         self.grid_size = grid_size
         self.square_size = square_size
         self.solved = 0 not in self.sudoku
+        self._recursions = 0
+        self.solution = None
 
     def __repr__(self):
-        return repr(self.sudoku)
+        return f"Class <Sudoku>, id: {id(self)}, solved: {self.solved}"
+
+    def __str__(self):
+        return str(self.sudoku)
+
+    def print(self):
+        return str(self.solution) if self.solved else str(self.sudoku)
+
+    def is_solved(self):
+        return f"{self.solved} after {self._recursions} recursions."
 
     def check(self, x, y, number):
         # Determine columns and rows
@@ -31,6 +42,8 @@ class Sudoku:
         return True
 
     def solve(self):
+        u"""Bactracking approach."""
+        self._recursions += 1
         for x in range(self.grid_size):
             for y in range(self.grid_size):
                 if self.sudoku[x, y] == 0:
@@ -40,7 +53,10 @@ class Sudoku:
                             self.solve()
                             self.sudoku[x, y] = 0
                     return
+
         self.solved = True
+        print(self.sudoku)
+        self.solution = np.copy(self.sudoku)
 
     @staticmethod
     def read_from_file(file_name, grid_size=9):
@@ -56,3 +72,18 @@ class Sudoku:
                 sudokus.append(sudoku)
                 sudoku = []
         return list(Sudoku(sudoku) for sudoku in sudokus)
+
+
+if __name__ == '__main__':
+    example = [[0, 0, 3, 0, 2, 0, 6, 0, 0],
+               [9, 0, 0, 3, 0, 5, 0, 0, 1],
+               [0, 0, 1, 8, 0, 6, 4, 0, 0],
+               [0, 0, 8, 1, 0, 2, 9, 0, 0],
+               [7, 0, 0, 0, 0, 0, 0, 0, 8],
+               [0, 0, 6, 7, 0, 8, 2, 0, 0],
+               [0, 0, 2, 6, 0, 9, 5, 0, 0],
+               [8, 0, 0, 2, 0, 3, 0, 0, 9],
+               [0, 0, 5, 0, 1, 0, 3, 0, 0]]
+    sudoku = Sudoku(example)
+    sudoku.solve()
+    sudoku.print()
